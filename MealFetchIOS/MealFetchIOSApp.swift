072f52ca7da46cbadea5691d49cc828af62820a5
DylanCHT <43015876+DylanCHT@users.noreply.github.com>
2023-09-22 20:32:21 -0400

    Initial Commit

---
+++ MealFetchIOS/MealFetchIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealFetchIOSApp.swift
+//  MealFetchIOS
+//
+//  Created by Dylan  Tao on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct MealFetchIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
